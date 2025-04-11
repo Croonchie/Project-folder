c9464813923984f1e6999a534ecedc2ddeb406b8
Joshua Salazar <salazarjoshua630@gmail.com>
2025-04-10 23:45:19 -0400

    version 2

--- javascript.js
+++ javascript.js
@@ -8,3 +8,14 @@ const student = {
       return `${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}`;
     }
   };
+
+  // JSON Operations
+const studentJSON = JSON.stringify(student);
+console.log(studentJSON);
+const studentParsed = JSON.parse(studentJSON);
+console.log(studentParsed);
+
+// Destructuring Assignment
+const { name, courses } = student;
+console.log(name);
+console.log(courses);
